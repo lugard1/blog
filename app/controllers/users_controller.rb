9f86bdce55834e859ad267609e4d0a33e9aedaaa
Lugard Agu <agulugard@gmail.com>
2023-07-28 17:15:38 +0100

    Create devise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user, only: %i[show edit update destroy]
+
   def index
     @users = User.all
   end
