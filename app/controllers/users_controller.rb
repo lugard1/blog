86823db91f500ef707b911a9fbd2f6fd2411a909
Lugard Agu <agulugard@gmail.com>
2023-07-28 20:45:32 +0100

    Update UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,4 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
-  before_action :set_user, only: %i[show edit update destroy]
-
   def index
     @users = User.all
   end
